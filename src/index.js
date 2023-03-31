e7595f34b03e95e8da23839ab0472711a3781f16
Kanat Juzbayev <kanat.juzbayev@gmail.com>
2023-03-31 14:53:50 +0600

    feat: build dist folder

--- src/index.js
+++ src/index.js
@@ -1,73 +1,75 @@
-import Article from './js/Article.js'
+import Article from "./js/Article.js";
 
-import data from './data.json' assert { type: "json" };
+import data from "./data.json";
 
-window.onload = function() {
-    //Tags
-    addTagClickHendler();
-    // Articles
-    showArticles(data);
-}
+console.log("Hello!");
+
+window.onload = function () {
+  //Tags
+  addTagClickHendler();
+  // Articles
+  showArticles(data);
+};
 
 const addTagClickHendler = () => {
-    document.querySelector('.strategies__tags').addEventListener('click', (e) => {
-        if (e.target.classList.contains('tag')) {
-            let clickedTag = e.target;
-            removeSelectedTag();
-            selectClickedTag(clickedTag);
+  document.querySelector(".strategies__tags").addEventListener("click", (e) => {
+    if (e.target.classList.contains("tag")) {
+      let clickedTag = e.target;
+      removeSelectedTag();
+      selectClickedTag(clickedTag);
 
-            if (clickedTag.innerText === 'All') {
-                showAllStrategies();
-            } else {
-                filetStrategiesByTag(clickedTag.innerText)
-            }
-        };
-    })
-}
+      if (clickedTag.innerText === "All") {
+        showAllStrategies();
+      } else {
+        filetStrategiesByTag(clickedTag.innerText);
+      }
+    }
+  });
+};
 
 const removeSelectedTag = () => {
-    let tags = document.querySelectorAll('.strategies__tags .tag');
-    tags.forEach(tag => {
-        tag.classList.remove('tag_selected');
-        tag.classList.add('tag_bordered');
-    });
-}
+  let tags = document.querySelectorAll(".strategies__tags .tag");
+  tags.forEach((tag) => {
+    tag.classList.remove("tag_selected");
+    tag.classList.add("tag_bordered");
+  });
+};
 
 const selectClickedTag = (tag) => {
-    tag.classList.remove('tag_bordered');
-    tag.classList.add('tag_selected');
-}
+  tag.classList.remove("tag_bordered");
+  tag.classList.add("tag_selected");
+};
 
 const showAllStrategies = () => {
-    let strategies = document.querySelectorAll('.strategies-wrapper .strategy');
-    strategies.forEach(strategy => {
-        strategy.classList.remove('strategy_hiden');
-    });
-}
+  let strategies = document.querySelectorAll(".strategies-wrapper .strategy");
+  strategies.forEach((strategy) => {
+    strategy.classList.remove("strategy_hiden");
+  });
+};
 
 const filetStrategiesByTag = (tag) => {
-    let strategies = document.querySelectorAll('.strategies-wrapper .strategy');
-    strategies.forEach(strategy => {
-        strategy.classList.add('strategy_hiden');
+  let strategies = document.querySelectorAll(".strategies-wrapper .strategy");
+  strategies.forEach((strategy) => {
+    strategy.classList.add("strategy_hiden");
 
-        strategy.querySelectorAll('.tag').forEach(strategyTag => {
-            if (strategyTag.innerText === tag) {
-                strategy.classList.remove('strategy_hiden');
-            }
-        })
-    })
-}
+    strategy.querySelectorAll(".tag").forEach((strategyTag) => {
+      if (strategyTag.innerText === tag) {
+        strategy.classList.remove("strategy_hiden");
+      }
+    });
+  });
+};
 
 const showArticles = (data) => {
-    let strategiesWrapper = document.querySelector('.strategies-wrapper');
-    strategiesWrapper.innerHTML = '';
+  let strategiesWrapper = document.querySelector(".strategies-wrapper");
+  strategiesWrapper.innerHTML = "";
 
-    let content = [];
-    data.forEach(artile => {
-        content.push(new Article(artile));
-    });
+  let content = [];
+  data.forEach((artile) => {
+    content.push(new Article(artile));
+  });
 
-    content.forEach(artile => {
-        strategiesWrapper.append(artile.generateArticle());
-    });
-}
+  content.forEach((artile) => {
+    strategiesWrapper.append(artile.generateArticle());
+  });
+};
